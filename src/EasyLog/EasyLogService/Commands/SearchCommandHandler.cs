96a0df3003386f24380d5fa4cb5a151994c992a4
Andre Kaufmann <Andre.Kaufmann@geutebrueck.com>
2020-07-15 09:10:27 +0200

    Added asynchronous call - searchquery and searchcompleted

--- src/EasyLog/EasyLogService/Commands/SearchCommandHandler.cs
+++ src/EasyLog/EasyLogService/Commands/SearchCommandHandler.cs
@@ -7,14 +7,17 @@ namespace EasyLogService.Commands
 
     public interface ISearchCommand
     {
-        Task Search(SearchRequest request);
+        Task Search(SearchRequest request, Action<string> completed);
     }
+
+    
 
     internal class SearchCommand : ISearchCommand
     {
-        public async Task Search(SearchRequest request)
+        public async Task Search(SearchRequest request, Action<string> completed)
         {
             await Task.Delay(1000);
+            completed(request.Query);
             Console.WriteLine("aha");
         }
     }
