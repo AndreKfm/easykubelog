70c721c71135d16c65d56706beb54f7dc3d09b07
Andre Kaufmann <Andre.Kaufmann@geutebrueck.com>
2020-08-28 23:12:13 +0200

    Added dockerfile, certificate yaml for deployment - running now in Kubernetes

--- src/EasyLog/EasyLogService/Program.cs
+++ src/EasyLog/EasyLogService/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
 
 namespace EasyLogService
 {
@@ -22,5 +24,7 @@ namespace EasyLogService
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
     }
 }
--- src/EasyLog/EasyLogService/Startup.cs
+++ src/EasyLog/EasyLogService/Startup.cs
@@ -139,7 +139,7 @@ namespace EasyLogService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
