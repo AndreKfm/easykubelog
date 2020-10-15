057224c79201bb529ff9a46d970ff2e3e0648afb
Andre Kaufmann <Andre.Kaufmann@geutebrueck.com>
2020-10-15 20:47:26 +0200

    Small refactorings

---
+++ src/EasyKubeLog/LoggingLibraries/FileTools/FileTools/StreamWriterWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileToolsClasses
+{
+    public interface IStreamWriterWrapper : IDisposable
+    {
+        public Task Write(string textLine);
+        public void Flush();
+    }
+
+    public class StreamWriterWrapperImpl : IStreamWriterWrapper
+    {
+        private readonly StreamWriter _writer;
+
+        public StreamWriterWrapperImpl(FileStream file)
+        {
+            _writer = new StreamWriter(file);
+        }
+
+        public async Task Write(string textLine)
+        {
+            await _writer.WriteAsync(textLine);
+        }
+
+        public void Flush()
+        {
+            _writer.Flush();
+        }
+
+        public void Dispose()
+        {
+            _writer?.Dispose();
+        }
+    }
+}
