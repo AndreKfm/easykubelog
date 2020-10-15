402875ae0ddbff4a1f0a37e0c35e34d67fb80ecc
Andre Kaufmann <Andre.Kaufmann@geutebrueck.com>
2020-10-15 23:29:04 +0200

    Added first entity

---
+++ src/EasyKubeLogDDD/bct/Scanner/src/Scanner.Domain/Ports/ReadLogEntryPort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scanner.Domain.Entities;
+
+namespace Scanner.Domain.Ports
+{
+    interface IReadLogEntryPort
+    {
+        // Returns default value if no new entry is available
+        LogEntryEntity ReadNext();
+    }
+}
